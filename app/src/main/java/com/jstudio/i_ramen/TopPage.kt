70937a60c5ebfd334a3e79e72c6b19af32f6d8df
JoeyYuas <joeyyuasa@gmail.com>
2019-01-16 00:04:59 +0900

    handlerによる画面遷移（テスト用）の追加

--- app/src/main/java/com/jstudio/i_ramen/TopPage.kt
+++ app/src/main/java/com/jstudio/i_ramen/TopPage.kt
@@ -1,12 +1,21 @@
 package com.jstudio.i_ramen
-
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class TopPage : AppCompatActivity() {
+
+    //private val mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_page)
+
+        //テスト用画面遷移（簡易）
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1000)
     }
 }
