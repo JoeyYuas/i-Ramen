391ac97d8c809d7083f8ff0c2672b9ed07ef5999
JoeyYuas <joeyyuasa@gmail.com>
2019-01-23 02:16:48 +0900

    ナビゲーションバー／ツールバーのモデルを作成
    取り急ぎ#2はクローズ

--- app/src/main/java/com/jstudio/i_ramen/MainActivity.kt
+++ app/src/main/java/com/jstudio/i_ramen/MainActivity.kt
@@ -2,11 +2,44 @@ package com.jstudio.i_ramen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_navigation.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_navigation)
+
+        //ActionBar(ツールバー）をセットする
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        drawer = findViewById(R.id.drawer_layout)
+        val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
     }
+
+    override fun onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+
 }
