2a821f394eab5a5bdf362296127a681e11a1d3d8
JoeyYuas <joeyyuasa@gmail.com>
2019-01-25 01:55:58 +0900

    fragment_mainの作成 fragmentをactivityに表示

--- app/src/main/java/com/jstudio/i_ramen/MainActivity.kt
+++ app/src/main/java/com/jstudio/i_ramen/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.jstudio.i_ramen
 
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.NavigationMenuItemView
+import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_navigation.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var drawer: DrawerLayout
+    lateinit var fragment : Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,9 @@ class MainActivity : AppCompatActivity() {
 
 
         drawer = findViewById(R.id.drawer_layout)
+        val navigationview : NavigationView = findViewById(R.id.nav_view)
+        navigationview.setNavigationItemSelectedListener (this)
+
         val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             drawer,
@@ -32,7 +41,22 @@ class MainActivity : AppCompatActivity() {
 
         drawer.addDrawerListener(toggle)
         toggle.syncState()
+
+        fragment = Main_Fragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+//        navigationview.setCheckedItem(R.id.nav_view)
+
     }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        fragment = Main_Fragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+
+        return true
+    }
+
 
     override fun onBackPressed(){
         if(drawer.isDrawerOpen(GravityCompat.START)){
